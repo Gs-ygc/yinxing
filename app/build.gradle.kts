c56a2ed04f8cf70030b6933f952df6063010c7a3
bjfwan <2632507193@qq.com>
2026-08-09 09:51:45 +0800

    chore: bump root preview version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.yinxing.launcher"
         minSdk = 24
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.10.0-root-preview.8"
+        versionCode = 25
+        versionName = "1.10.0-root-preview.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
